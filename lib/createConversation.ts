52773902d631000ec679aba3a2dfb6e1377162d7
Rodrigo Moreira Santos <rodrigo.moreirasantos@botpress.com>
2025-06-06 14:22:21 -0400

    fix: Request to return value

--- lib/createConversation.ts
+++ lib/createConversation.ts
@@ -1,21 +1,23 @@
-import { createUser } from "./createUser";
-
-export async function createConversation() {
-  const user = await createUser();
+export async function createConversation(key: string) {
   const url =
     "https://chat.botpress.cloud/9b202e36-364d-4ef5-b2c3-f39a1aeb235a/conversations";
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
-      "x-user-key": `${user.key}`,
+      "x-user-key": `${key}`,
       "content-type": "application/json",
     },
-    body: JSON.stringify({ id: "conv_27309127391283761209371290" }),
+    body: JSON.stringify({ id: "" }),
   };
+  try {
+    const res = await fetch(url, options);
+    const json = await res.json();
+    console.log("CONVERSATION OK");
 
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error(err));
+    return json;
+  } catch (err) {
+    console.error("CONVERSATION FAIL", err);
+    return undefined;
+  }
 }
