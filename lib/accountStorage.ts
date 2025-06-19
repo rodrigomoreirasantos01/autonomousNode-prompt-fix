0c6c78fedf9e7c5016fdc9c8d7bd3155cd14b1f1
Rodrigo Moreira Santos <rodrigo.moreirasantos@botpress.com>
2025-06-19 15:01:04 -0400

    fix: change token and id endpoint

--- lib/accountStorage.ts
+++ lib/accountStorage.ts
@@ -1,17 +1,17 @@
 export async function accountStorage(email: string, password: string) {
   const url =
     //change table id later
-    "https://api.botpress.cloud/v1/tables/table_01JX46SX2DGJP8Q2W020DEBZZR/rows/upsert";
+    "https://api.botpress.cloud/v1/tables/table_01JX45TDXA4C37VM0F9GMZJ7QV/rows/upsert";
 
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
-      "x-bot-id": "88ab7dec-be4b-4cf6-a94c-5b3f971c4739",
-      "x-workspace-id": "wkspace_01JQ7B996FTQSH23XHV8RT1B0N",
+      "x-bot-id": "06f70f02-c553-4c75-b519-4499f676164c",
+      "x-workspace-id": "wkspace_01J5XF54MAQBP0WQVV9PG06ABE",
       "content-type": "application/json",
       //temporary key, we will make another one later
-      authorization: "Bearer bp_pat_BFgnlcVQ79QYntB80tXNqxW78Z1TyF5K5zCr",
+      authorization: "Bearer bp_pat_wzspJtpOUslJTVYDEowzouh51EkViDb5ZdMw",
     },
     body: JSON.stringify({
       rows: [{ id: 1, email: email, password: password }],
--- lib/findStoredAccount.ts
+++ lib/findStoredAccount.ts
@@ -1,16 +1,16 @@
 export async function findStoredAccount(email: string, password: string) {
   const url =
     //change table id later
-    "https://api.botpress.cloud/v1/tables/table_01JX46SX2DGJP8Q2W020DEBZZR/rows/find";
+    "https://api.botpress.cloud/v1/tables/table_01JX45TDXA4C37VM0F9GMZJ7QV/rows/find";
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
-      "x-bot-id": "88ab7dec-be4b-4cf6-a94c-5b3f971c4739",
-      "x-workspace-id": "wkspace_01JQ7B996FTQSH23XHV8RT1B0N",
+      "x-bot-id": "06f70f02-c553-4c75-b519-4499f676164c",
+      "x-workspace-id": "wkspace_01J5XF54MAQBP0WQVV9PG06ABE",
       "content-type": "application/json",
       //temporary key, we will make another one later
-      authorization: "Bearer bp_pat_BFgnlcVQ79QYntB80tXNqxW78Z1TyF5K5zCr",
+      authorization: "Bearer bp_pat_wzspJtpOUslJTVYDEowzouh51EkViDb5ZdMw",
     },
     body: JSON.stringify({
       limit: 1,
